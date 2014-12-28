0e0989071f2b18bfbaf743f94ce4e9501d3d41f3
drevil66635 <annesteenbeek@gmail.com>
2014-12-28 23:19:44 +0100

    inserted right text and added bedrijfsyoga

--- js/custom.js
+++ js/custom.js
@@ -2,7 +2,7 @@
 
 jQuery(document).ready(function() {
     jQuery('.face').addClass("hidden2").viewportChecker({
-        classToAdd: 'visible2 animated fadeInRight',
+        classToAdd: 'visible2 animated fadeInLeft',
         offset: 100
        });
 });
